0059b763ad0cd6461786d933ff6508d7260e25fc
Levon-MJ <Levon093612mail.ru>
2021-10-10 12:10:07 +0300

    ifExample, add IntExample

--- src/homework/Triangle.java
+++ src/homework/Triangle.java
@@ -13,7 +13,7 @@ public class Triangle {
         }
         for (int i = 0; i < 5; i++) {
             for (int j = 5; j > i; j--) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
